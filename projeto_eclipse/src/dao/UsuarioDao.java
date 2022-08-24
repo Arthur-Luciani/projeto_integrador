aba26d2f51eecda5b1096615b805fee8248c36ed
Arthur <arthurflcorrea@gmail.com>
2022-08-24 12:07:10 -0300

    Alterações nos métodos de validação

--- projeto_eclipse/src/dao/UsuarioDao.java
+++ projeto_eclipse/src/dao/UsuarioDao.java
@@ -6,26 +6,33 @@ import java.sql.SQLException;
 
 import model.Usuario;
 
+public class UsuarioDao extends BD {
 
-public class UsuarioDao extends BD{
-	
 	public UsuarioDao() throws SQLException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public boolean verificacao(Usuario usuario) throws SQLException {
-		PreparedStatement  ps = conexao.prepareStatement("select login from Usuarios where login=? and senha=?");
-		ps.setString(1,usuario.getLogin());
-		ps.setString(2,usuario.getSenha());
-	    
-	    ResultSet rs = ps.executeQuery();
-	    if (rs.isFirst()) {
-	    	return true;
+	public Usuario verificacao(Usuario usuario) throws SQLException {
+		PreparedStatement ps = conexao.prepareStatement("select * from Usuarios where login=? and senha=?");
+		ps.setString(1, usuario.getLogin());
+		ps.setString(2, usuario.getSenha());
+
+		ResultSet rs = ps.executeQuery();
+		if (rs.isFirst()) {
+			Usuario usuarioLogado = null; // pegar os dados da consulta
+					
+					int id = rs.getInt("id");
+					String login = rs.getString("login");
+					
+					String senha = rs.getString("senha");
+					
+					usuarioLogado.setPermissao(rs.getBoolean("permissao"));
+			return usuarioLogado;
 		} else {
-			return false;
+			return null;
 		}
-	    
+
 	}
-	
+
 }

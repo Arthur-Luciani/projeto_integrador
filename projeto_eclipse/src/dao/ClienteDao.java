efd2e0c249e0ca2b5993b0a5a2a5b8707f8969eb
Arthur Correa <arthurflcorrea@gmail.com>
2022-10-05 18:16:17 -0300

    Alterações no método de cadastrar cliente

--- projeto_eclipse/src/dao/ClienteDao.java
+++ projeto_eclipse/src/dao/ClienteDao.java
@@ -19,55 +19,29 @@ public class ClienteDao {
 		PreparedStatement ps;
 		try {
 			ps = conexao
-					.prepareStatement("select cpf from cliente where cpf=?");
-			ps.setString(1, cliente.getCpf());
-			ResultSet rs = ps.executeQuery();	
+					.prepareStatement("insert into mydb.endereco (bairro, rua, cidade, cep, id_estado) "
+							+ "values ('fortaleza','cacilda','blumenau','23432', (select id from mydb.estados where nome like 'Acre') );");
 			
-			if (!rs.next()) {
-				ps = conexao
-						.prepareStatement("select id from estados where nome=?");
-				ps.setString(1, cliente.getEstado());
-				rs = ps.executeQuery();
-				int idEstado=0;
-				if (rs.next()) {
-					idEstado =rs.getInt("id");
-				}
-				
-				ps = conexao
-						.prepareStatement("insert into endereco (bairro, rua, cidade, cep, id_estado)"
-								+ "select ?,?,?,?, id "
-								+ "from estados where nome = ?");
-				ps.setString(1, cliente.getBairro());
-				ps.setString(2, cliente.getRua());
-				ps.setString(3, cliente.getCidade());
-				ps.setString(4, cliente.getCep());
-				ps.setInt(5, idEstado);
-				ps.execute();
-				
-				
-				ps = conexao
-						.prepareStatement("select id_endereco from endereco");
-				rs = ps.executeQuery();
-				int idEndereco=0;
-				while (rs.next()) {
-					idEndereco = rs.getInt("id_endereco");
-				}
-				
-				ps = conexao
-						.prepareStatement("inset into cliente (nome, cpf, email, data_nasc, id_endereco)"
-								+ "values (?,?,?,?,?)");
-				ps.setString(1, cliente.getNome());
-				ps.setString(2, cliente.getCpf());
-				ps.setString(3, cliente.getEmail());
-				ps.setDate(4, java.sql.Date.valueOf(cliente.getDataNascimento()));
-				ps.setInt(5, idEndereco);
-				ps.execute();
-				BD.fechaConexao();
-				return true;
-			}
+			
+			/*
+			ps.setString(1, cliente.getBairro());
+			ps.setString(2, cliente.getRua());
+			ps.setString(3, cliente.getCidade());
+			ps.setString(4, cliente.getCep());
+			ps.setString(5, cliente.getEstado());
+			
+			*/
+			ps.execute();
+			
+			
+			
+			BD.fechaConexao();
+			return true;
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 		
 		return false;

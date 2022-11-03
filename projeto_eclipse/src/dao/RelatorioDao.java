13b6a0a9015e428f12e36543e9305fc8d8107edd
Arthur <arthurflcorrea@gmail.com>
2022-11-03 10:24:30 -0300

    template tela relatorios

---
+++ projeto_eclipse/src/dao/RelatorioDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public class RelatorioDao {
+	private Connection conexao;
+	
+	public RelatorioDao() {}
+	
+	public ArrayList<E> name() {
+		
+	}
+}
